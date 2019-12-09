5deb4b0a627e3557efd65ddbf2f9b2fd4d881098
eudomars <leomarsepulvedar@gmail.com>
2019-12-09 15:31:17 -0430

    Se agrego el nombre del sistema y se agrego la base de dato, con su respectivo usuario

--- core/configAPP.php
+++ core/configAPP.php
@@ -1,10 +1,10 @@
 <?php
 // Conexión 1---------------------------------------------------------------------------------------------------------------------
 const SERVER = "localhost options='--client_encoding=UTF8'";
-const DBNAME = 'nomina';
+const DBNAME = 'extranet';
 const DBUSER = 'postgres';
-const DBPASS = 'sistemas-cpnb';
-const DBPORT = 5432; // Solo para base de datos PostgreSQL
+const DBPASS = '1qaz2wsx';
+const DBPORT = 5433; // Solo para base de datos PostgreSQL
 // Para Bases de datos MySQL
 // const SGBD = 'mysql:host='.SERVER.';dbname='.DBNAME;
 // Para Bases de datos PostgreSQL
@@ -14,9 +14,9 @@ const SGBD = 'pgsql:host='.SERVER.';port='.DBPORT.';dbname='.DBNAME;
 // No modificar
 const METHOD = 'AES-256-CBC';
 // Colocar aquí las iniciales del sistema, incluir algunos simbolos, esto antes de registrar una cuenta de usuario en la base de datos, luego de registrar algo, no se puede modificar esta constante, ya que si se hace, no se podra recuperar el valor de la contraseña encriptada
-const SECRET_KEY = '$SW@001';
+const SECRET_KEY = '$SWE@001';
 // Colocar solo números, cualquier valor, esto antes de registrar una cuenta de usuario en la base de datos, luego de registrar algo, no se puede modificar esta constante, ya que si se hace, no se podra recuperar el valor de la contraseña encriptada
-const SECRET_IV = '151519';
+const SECRET_IV = '152518';
 
 // Segunda conexión
 const SERVER2 = "ipserver options='--client_encoding=UTF8'";
